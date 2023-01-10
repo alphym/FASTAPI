899c8229b0707df981bbb298608887b46e3d3689
alphym <alphy108@gmail.com>
2023-01-10 10:19:35 +0200

    Added about page in api

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+
 main  = FastAPI()
+
 @main.get('/') #this the path/route
 def index():
     return "This is the front page"
+@main.get('about')
+def about():
+    return"this is about"
